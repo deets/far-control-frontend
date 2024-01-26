1c3d01833b33d6003c51f83d177228c464240d25
deets <deets@web.de>
2024-01-26 21:24:19 +0100

    Render with standard widgets, experiment with status bar

--- src/render.rs
+++ src/render.rs
@@ -1,3 +1,6 @@
+use std::f64::consts::TAU;
+
+use egui::plot::{Legend, Line, LineStyle, Plot, PlotPoints};
 use egui::{Color32, Rect, Stroke, Ui, Vec2};
 
 use crate::layout;
@@ -16,31 +19,41 @@ fn split_rect_horizontally_at(rect: &Rect, split: f32) -> (Rect, Rect) {
 }
 
 fn render_header(ui: &mut Ui, state: &State) {
-    let desired_size = [
-        ui.available_width(),
-        ui.available_height() * layout::header::MARGIN,
-    ];
-    let (_id, rect) = ui.allocate_space(desired_size.into());
-    ui.painter().rect(rect, 0.0, Color32::RED, Stroke::NONE);
-    let (lr, rr) = split_rect_horizontally_at(&rect, 0.5);
-    let (active, background, active_rect) = match state.active {
-        ActiveTab::Observables => (
-            layout::colors::OBSERVABLES,
-            muted(layout::colors::LAUNCHCONTROL),
-            lr,
-        ),
-        ActiveTab::LaunchControl => (
-            layout::colors::LAUNCHCONTROL,
-            muted(layout::colors::OBSERVABLES),
-            rr,
-        ),
-    };
-    let active = match state.control {
-        ControlArea::Details => muted(active),
-        ControlArea::Tabs => active,
-    };
-    ui.painter().rect(rect, 0.0, background, Stroke::NONE);
-    ui.painter().rect(active_rect, 0.0, active, Stroke::NONE);
+    let mut active_panel = state.active.clone();
+    ui.horizontal(|ui| {
+        ui.selectable_value(&mut active_panel, ActiveTab::Observables, "Observables");
+        ui.selectable_value(
+            &mut active_panel,
+            ActiveTab::LaunchControl,
+            "Launch Control",
+        );
+    });
+    // ui.sel
+    // let desired_size = [
+    //     ui.available_width(),
+    //     ui.available_height() * layout::header::MARGIN,
+    // ];
+    // let (_id, rect) = ui.allocate_space(desired_size.into());
+    // ui.painter().rect(rect, 0.0, Color32::RED, Stroke::NONE);
+    // let (lr, rr) = split_rect_horizontally_at(&rect, 0.5);
+    // let (active, background, active_rect) = match state.active {
+    //     ActiveTab::Observables => (
+    //         layout::colors::OBSERVABLES,
+    //         muted(layout::colors::LAUNCHCONTROL),
+    //         lr,
+    //     ),
+    //     ActiveTab::LaunchControl => (
+    //         layout::colors::LAUNCHCONTROL,
+    //         muted(layout::colors::OBSERVABLES),
+    //         rr,
+    //     ),
+    // };
+    // let active = match state.control {
+    //     ControlArea::Details => muted(active),
+    //     ControlArea::Tabs => active,
+    // };
+    // ui.painter().rect(rect, 0.0, background, Stroke::NONE);
+    //ui.painter().rect(active_rect, 0.0, active, Stroke::NONE);
 }
 
 fn render_body(ui: &mut Ui, state: &State) {
@@ -57,7 +70,52 @@ fn render_body(ui: &mut Ui, state: &State) {
     ui.painter().rect(rect, 0.0, color, Stroke::NONE);
 }
 
+fn render_status(ui: &mut Ui, state: &State) {
+    ui.horizontal(|ui| {
+        ui.spinner();
+        ui.ctx().request_repaint();
+
+        let elapsed = state.elapsed();
+        let mut plot = Plot::new("lines_demo")
+            .height(ui.available_height())
+            .width(ui.available_height())
+            .show_axes([false, false])
+            .show_background(false);
+        plot = plot.data_aspect(1.0);
+        plot.show(ui, |ui| {
+            let steps = 16;
+            ui.line(
+                Line::new(PlotPoints::from_explicit_callback(
+                    move |x| 0.5 * (TAU * (x + elapsed.as_secs_f64())).sin(),
+                    0.0..=1.0,
+                    steps,
+                ))
+                .color(Color32::from_rgb(200, 100, 100))
+                .style(LineStyle::Solid)
+                .name("wave"),
+            );
+        });
+    });
+}
+
 pub fn render(ui: &mut Ui, state: &State) {
-    render_header(ui, state);
-    render_body(ui, state);
+    egui::TopBottomPanel::top("top_panel")
+        .resizable(false)
+        .min_height(ui.spacing().interact_size.y * 2.0)
+        .show_inside(ui, |ui| {
+            ui.vertical_centered(|ui| {
+                render_header(ui, state);
+            });
+        });
+    egui::TopBottomPanel::bottom("bottom_panel")
+        .resizable(false)
+        .min_height(ui.spacing().interact_size.y * 2.0)
+        .show_inside(ui, |ui| {
+            ui.vertical_centered(|ui| {
+                render_status(ui, state);
+            });
+        });
+    egui::CentralPanel::default().show_inside(ui, |ui| {
+        render_body(ui, state);
+    });
 }
