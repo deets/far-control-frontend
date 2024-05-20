994343d6e7fc248b156adab8e0aad7d704c2b98a
deets <deets@web.de>
2024-05-20 14:42:06 +0200

    Moving telemetry into thread and send messages

--- src/bin/nrf-test.rs
+++ src/bin/nrf-test.rs
@@ -1,9 +1,24 @@
-use control_frontend::telemetry::setup_telemetry;
+use control_frontend::{
+    rqprotocol::Node,
+    telemetry::{setup_telemetry, Config},
+};
 use log::info;
 
 fn main() -> anyhow::Result<()> {
     simple_logger::init_with_env().unwrap();
     info!("NRF TEST");
-    setup_telemetry()?;
+    let mut telemetry = setup_telemetry(
+        [Config {
+            node: Node::RedQueen(b'A'),
+            channel: 0,
+        }]
+        .into_iter(),
+    )?;
+    for _ in 0..100 {
+        telemetry.recv(|data| {
+            println!("{:?}", data);
+        });
+    }
+
     Ok(())
 }
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,18 +1,26 @@
 use std::path::PathBuf;
-use std::time::Instant;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
 use anyhow::anyhow;
+use crossbeam_channel::{unbounded, Receiver, Sender};
 use embedded_hal::blocking::spi::Transfer;
 use embedded_hal::digital::v2::OutputPin;
-use embedded_nrf24l01::StandbyMode;
 use embedded_nrf24l01::{Configuration, CrcMode, DataRate, NRF24L01};
+use embedded_nrf24l01::{RxMode, StandbyMode};
 use linux_embedded_hal::spidev::spidevioctl::spi_ioc_transfer;
 use linux_embedded_hal::{
     gpio_cdev::{Chip, LineRequestFlags},
     spidev::{SpiModeFlags, Spidev, SpidevOptions},
     CdevPin, CdevPinError,
 };
-use log::{debug, info};
+use log::{error, warn};
+
+use crate::rqprotocol::Node;
+
+type SpiError = embedded_nrf24l01::Error<std::io::Error>;
+type NRFStandby = StandbyMode<NRF24L01<CdevPinError, CEPin, NullPin, SpiWrapper>>;
+type NRFRx = RxMode<NRF24L01<CdevPinError, CEPin, NullPin, SpiWrapper>>;
 
 struct NullPin {}
 impl OutputPin for NullPin {
@@ -60,9 +68,6 @@ impl Transfer<u8> for SpiWrapper {
     }
 }
 
-type SpiError = embedded_nrf24l01::Error<std::io::Error>;
-type NRFStandby = StandbyMode<NRF24L01<CdevPinError, CEPin, NullPin, SpiWrapper>>;
-
 fn create_spi(spi_dev_path: &str) -> std::io::Result<Spidev> {
     let mut spi = Spidev::open(spi_dev_path)?;
     let options = SpidevOptions::new()
@@ -83,72 +88,155 @@ fn setup_nrf(ce_pin: CEPin, spi: SpiWrapper) -> core::result::Result<NRFStandby,
     nrf24.set_tx_addr(&b"RQARQ"[..])?;
     nrf24.set_rx_addr(0, &b"RQARQ"[..])?;
     nrf24.set_pipes_rx_lengths(&[Some(32); 6])?;
-    nrf24.set_frequency(0)?;
     Ok(nrf24)
 }
 
-fn read_nrf(nrf: NRFStandby) -> core::result::Result<(), SpiError> {
-    let mut rx = nrf.rx().unwrap();
-    let start = Instant::now();
-    let mut count = 0;
-
-    loop {
-        loop {
-            if let Some(_) = rx.can_read().unwrap() {
-                break;
-            }
-        }
-        let data = rx.read().unwrap();
-        let raw: &[u8] = &data;
-        count += raw.len() * 8;
-        let kbit_per_second = count as f64 / (Instant::now() - start).as_secs_f64();
-        print!(
-            "{}\nkbit: {:.3}\n",
-            std::str::from_utf8(raw).unwrap(),
-            kbit_per_second
-        );
-    }
-}
-
-pub fn setup_telemetry() -> anyhow::Result<()> {
-    let mut chip = Chip::new::<PathBuf>("/dev/gpiochip0".into())?;
-    let ce_pin0 = chip
-        .get_line(22)?
+fn create_nrf_module(chip: &mut Chip, ce_pin: u32, device: &str) -> anyhow::Result<NRFStandby> {
+    let ce_pin = chip
+        .get_line(ce_pin)?
         .request(LineRequestFlags::OUTPUT, 0, "e32linux")?;
-    let ce_pin0 = CEPin {
-        pin: CdevPin::new(ce_pin0)?,
+    let ce_pin = CEPin {
+        pin: CdevPin::new(ce_pin)?,
     };
-    let ce_pin1 = chip
-        .get_line(25)?
-        .request(LineRequestFlags::OUTPUT, 0, "e32linux")?;
-    let ce_pin1 = CEPin {
-        pin: CdevPin::new(ce_pin1)?,
-    };
-    let ce_pin2 = chip
-        .get_line(26)?
-        .request(LineRequestFlags::OUTPUT, 0, "e32linux")?;
-    let ce_pin2 = CEPin {
-        pin: CdevPin::new(ce_pin2)?,
-    };
-    let ce_pin3 = chip
-        .get_line(27)?
-        .request(LineRequestFlags::OUTPUT, 0, "e32linux")?;
-    let ce_pin3 = CEPin {
-        pin: CdevPin::new(ce_pin3)?,
-    };
-
-    let spi00 = SpiWrapper(create_spi("/dev/spidev0.0")?);
-    let spi01 = SpiWrapper(create_spi("/dev/spidev0.1")?);
-    let spi10 = SpiWrapper(create_spi("/dev/spidev1.0")?);
-    let spi11 = SpiWrapper(create_spi("/dev/spidev1.1")?);
-
-    let mut nrf24 = match setup_nrf(ce_pin1, spi01) {
+    let spi = SpiWrapper(create_spi(device)?);
+    let nrf24 = match setup_nrf(ce_pin, spi) {
         Ok(nrf) => Ok(nrf),
         Err(_) => Err(anyhow!("Can't setup NRF24")),
     }?;
-    match read_nrf(nrf24) {
-        Ok(_) => Ok(()),
-        Err(_) => Err(anyhow!("Can't receive with NRF24")),
-    }?;
-    Ok(())
+    Ok(nrf24)
+}
+
+fn enumerate_nrf_modules(chip: &mut Chip) -> impl Iterator<Item = NRFStandby> {
+    let mut nrfs = vec![];
+    for (ce_pin, device) in [(22, "/dev/spidev0.0")] {
+        match create_nrf_module(chip, ce_pin, device) {
+            Ok(nrf) => nrfs.push(nrf),
+            Err(err) => {
+                error!("Can't setup NRF on {}, {:?}", device, err);
+            }
+        }
+    }
+    nrfs.into_iter()
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Config {
+    pub node: Node,
+    pub channel: u8,
+}
+
+struct TelemetryConnection {
+    nrf: NRFRx,
+    node: Node,
+}
+
+#[derive(Debug)]
+pub struct TelemetryData {
+    pub source: Node,
+    pub data: Vec<u8>,
+}
+
+pub struct TelemetryEndpoint {
+    worker: Option<JoinHandle<()>>,
+    command_receiver: Receiver<TelemetryData>,
+    running: Arc<Mutex<bool>>,
+}
+
+impl TelemetryEndpoint {
+    pub fn recv(&mut self, callback: impl FnOnce(TelemetryData)) {
+        match self.command_receiver.recv() {
+            Ok(data) => {
+                callback(data);
+            }
+            Err(err) => {
+                error!("Can't read from cb channel: {:?}", err);
+            }
+        }
+    }
+
+    fn quit(&mut self) {
+        {
+            let mut running = self.running.lock().unwrap();
+            *running = false;
+        }
+        // See https://stackoverflow.com/questions/57670145/how-to-store-joinhandle-of-a-thread-to-close-it-later
+        self.worker.take().map(JoinHandle::join);
+    }
+}
+
+impl Drop for TelemetryEndpoint {
+    fn drop(&mut self) {
+        self.quit();
+    }
+}
+fn work(
+    sender: Sender<TelemetryData>,
+    mut connections: Vec<TelemetryConnection>,
+    running: Arc<Mutex<bool>>,
+) {
+    loop {
+        for conn in connections.iter_mut() {
+            while let Some(_) = conn.nrf.can_read().unwrap() {
+                let payload = conn.nrf.read().unwrap();
+                let data: &[u8] = &payload;
+                let data = TelemetryData {
+                    source: conn.node,
+                    data: data.into(),
+                };
+                sender.send(data).expect("crossbeam not working");
+            }
+        }
+        {
+            let mut r = running.lock().unwrap();
+            if !*r {
+                break;
+            }
+        }
+    }
+}
+
+pub fn setup_telemetry(configs: impl Iterator<Item = Config>) -> anyhow::Result<TelemetryEndpoint> {
+    let mut chip = Chip::new::<PathBuf>("/dev/gpiochip0".into())?;
+    let nrf_modules = enumerate_nrf_modules(&mut chip).collect::<Vec<NRFStandby>>();
+    let configs = configs.collect::<Vec<Config>>();
+    if nrf_modules.len() < configs.len() {
+        warn!(
+            "Not enough modules for configuration, only configuring the first {}",
+            configs.len()
+        );
+    }
+    let mut connections = vec![];
+    for (config, mut nrf) in configs.into_iter().zip(nrf_modules.into_iter()) {
+        match nrf.set_frequency(config.channel) {
+            Ok(_) => {
+                if let Some(conn) = match nrf.rx() {
+                    Ok(rx_nrf) => Some(TelemetryConnection {
+                        nrf: rx_nrf,
+                        node: config.node,
+                    }),
+                    Err(_) => {
+                        warn!("Can't get module into RX mode");
+                        None
+                    }
+                } {
+                    connections.push(conn);
+                };
+            }
+            Err(_) => {
+                warn!("Can't set frequency for {:?}", config);
+            }
+        }
+    }
+    let running = Arc::new(Mutex::new(true));
+    let worker_running = running.clone();
+    let (command_sender, command_receiver) = unbounded::<TelemetryData>();
+    let handle = thread::spawn(move || {
+        work(command_sender, connections, worker_running);
+    });
+
+    Ok(TelemetryEndpoint {
+        command_receiver,
+        worker: Some(handle),
+        running,
+    })
 }
